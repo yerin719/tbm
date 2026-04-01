9bd91022ee0e821508eba61dd2533dec00ebac96
yerin719 <woow0719@gmail.com>
2026-04-01 21:51:21 +0900

    feat: 전역 스타일에 overflow-x 속성 추가 및 레이아웃 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       lang="ko"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col font-sans">
+      <body className="min-h-full flex flex-col font-sans overflow-x-clip">
         <IntroExitProvider>{children}</IntroExitProvider>
       </body>
     </html>
